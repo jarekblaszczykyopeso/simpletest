04a25bb35c654de7519461d812ac29e134fa9780
jarekblaszczykyopeso <jaroslaw.blaszczyk@yopeso.com>
2017-09-12 15:38:31 +0300

    just commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,10 @@
 public class MainClass {
     public static void main (String[]  aaa) {
         System.out.println("no hejka");
+
+        int i =3;
+        int j=6;
+        while(9>3){}
     }
 }
+
